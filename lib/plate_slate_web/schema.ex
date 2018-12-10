8115fde449fc355c91ad040b755e1a187bb310c7
SumitPugalia <sumit@mev.ae>
2018-12-10 12:44:10 +0400

    add the description for docs

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -2,6 +2,7 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
   alias PlateSlate.{Menu, Repo}
 
+  @desc "The list of available items on the menu"
   query do
   	field :menu_items, list_of(:menu_item) do
       resolve fn _, _, _ -> {:ok, Repo.all(Menu.Item)} end
@@ -9,8 +10,10 @@ defmodule PlateSlateWeb.Schema do
   end
 
   object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
+    field :id, :id, description: "primary id"
+    field :name, :string, description: "name of the item"
+    field :description, :string, description: "describe the item"
+    field :price, :string, description: "price of the item"
+    field :added_on, :string, description: "created at"
   end
 end
