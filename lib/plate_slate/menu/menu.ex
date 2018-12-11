947749cf2e09f20157756fdd8251c01a34eb22f6
SumitPugalia <sumit@mev.ae>
2018-12-11 16:31:14 +0400

    different way of writing query

--- lib/plate_slate/menu/menu.ex
+++ lib/plate_slate/menu/menu.ex
@@ -2,11 +2,11 @@ defmodule PlateSlate.Menu.Menu do
 	alias PlateSlate.{Menu, Repo}
   import Ecto.Query
   def list_items(%{matching: name}) when is_binary(name) do 
-  	# query = from t in Menu.Item, where: ilike(t.name, name)
-  	# Repo.all(query)
-    Menu.Item
-    |> where([m], ilike(m.name, ^"%#{name}%"))
-    |> Repo.all
+  	query = from t in Menu.Item, where: ilike(t.name, ^name)
+  	Repo.all(query)
+    # Menu.Item
+    # |> where([m], ilike(m.name, ^"%#{name}%"))
+    # |> Repo.all
   end
   
   def list_items(_) do
